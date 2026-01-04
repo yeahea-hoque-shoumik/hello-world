559eba9bee74f50bf4246aa26a58debe1411b2b8
shoumik <yeahea.hoque@tallykhata.com>
2026-01-04 07:14:18 +0600

    dutch national flag

---
+++ coding-interview-patterns/02-two-pointers/sort_colors.py
@@ -0,0 +1,17 @@
+def sort_colors(nums):
+    low,mid,high=0,0,len(nums)-1
+    while mid<=high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+
+#also called Dutch national flag problem
+if __name__ == '__main__':
+    nums = [1,2,0]
+    print(sort_colors(nums))
