51ab3c3dc44643e59b73242c2fd0da345ae9cd74
shoumik <yeaheahoqueshoumik@gmai.com>
2026-02-27 21:53:30 +0600

    linked list cycle find

---
+++ coding-interview-patterns/03-fast-and-slow-pointers/linked_list_cycle_find.py
@@ -0,0 +1,8 @@
+def hasCycle(self, head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
